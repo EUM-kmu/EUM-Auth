e6a28c93e94d6b4d62d597eba17afa3d4bf9bb7a
solhee-hwang <solhee0912@kookmin.ac.kr>
2024-03-26 01:12:57 +0900

    Fix : cors 에러 해결

--- src/main/java/com/eum/auth/EumAuthApplication.java
+++ src/main/java/com/eum/auth/EumAuthApplication.java
@@ -16,14 +16,18 @@ public class EumAuthApplication {
     public static void main(String[] args) {
         SpringApplication.run(EumAuthApplication.class, args);
     }
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**").allowedOrigins("*");
-            }
-        };
-    }
+//    @Bean
+//    public WebMvcConfigurer corsConfigurer() {
+//        return new WebMvcConfigurer() {
+//            @Override
+//            public void addCorsMappings(CorsRegistry registry) {
+//                registry.addMapping("/**").allowedOrigins("http://localhost:3000").allowedMethods("GET", "POST", "PUT", "DELETE")
+////                        .allowedHeaders("Authorization", "Content-Type")
+////                        .exposedHeaders("Custom-Header")
+//                        .allowCredentials(true)
+//                        .maxAge(3600);;
+//            }
+//        };
+//    }
 
 }
