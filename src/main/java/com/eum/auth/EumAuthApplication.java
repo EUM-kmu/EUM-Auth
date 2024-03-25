b632ce1c00b81da52531cc7a11263a70af5718e9
solhee-hwang <solhee0912@kookmin.ac.kr>
2024-03-25 16:07:32 +0900

    fix : cors 해결

--- src/main/java/com/eum/auth/EumAuthApplication.java
+++ src/main/java/com/eum/auth/EumAuthApplication.java
@@ -4,6 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 //@EnableDiscoveryClient
 @EnableFeignClients
@@ -12,6 +15,15 @@ public class EumAuthApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(EumAuthApplication.class, args);
+    }
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("*");
+            }
+        };
     }
 
 }
