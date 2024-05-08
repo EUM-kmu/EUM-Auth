307d53eb8b81a7d594c0cc27308d9dafbd791384
solhee-hwang <solhee0912@kookmin.ac.kr>
2024-05-08 15:43:59 +0900

    chore : 404 해결

--- src/main/java/com/eum/auth/controller/HealthCheck.java
+++ src/main/java/com/eum/auth/controller/HealthCheck.java
@@ -1,8 +1,9 @@
-package com.eum.auth.controller.DTO;
+package com.eum.auth.controller;
 
 import io.swagger.v3.oas.annotations.Hidden;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequestMapping
@@ -12,5 +13,10 @@ public class HealthCheck {
     @GetMapping
     public String check(){
         return "ok";
+    }
+    @Hidden
+    @GetMapping("favicon.ico")
+    @ResponseBody
+    public void returnNoFavicon() {
     }
 }
