6644a7d296b382612f44d7078f8259879ac190d7
solhee-hwang <solhee0912@kookmin.ac.kr>
2024-05-08 10:53:30 +0900

    feat : 로그아웃 추가

--- src/main/java/com/eum/auth/config/jwt/JwtTokenProvider.java
+++ src/main/java/com/eum/auth/config/jwt/JwtTokenProvider.java
@@ -42,6 +42,7 @@ public class JwtTokenProvider {
     private static final String ROLE = "role";
     private static final String BEARER_TYPE = "Bearer";
     private static final String PREVIOUS_USERID = "previousUserId";
+    private static final String DELETED = "deleted";
     private static final long ACCESS_TOKEN_EXPIRE_TIME = 12 * 60 * 60 * 1000L;
     private static final long REFRESH_TOKEN_EXPIRE_TIME = 7 * 24 * 60 * 60 * 1000L;    // 7일
     @Autowired
@@ -66,6 +67,7 @@ public class JwtTokenProvider {
         claims.put(UID, user.getUid());
         claims.put(ROLE, user.getRole());
         claims.put(PREVIOUS_USERID, user.getPreviousUserId());
+        claims.put(DELETED, user.isDeleted());
 
         UserResponse.TokenInfo tokenInfo = generateToken(user.getUserId(),user.getRole(), claims);
         if(user.getRole().equals(ROLE_USER)) {
@@ -81,6 +83,7 @@ public class JwtTokenProvider {
         claims.put(UID, user.getUid());
         claims.put(ROLE, user.getRole());
         claims.put(PREVIOUS_USERID, user.getPreviousUserId());
+        claims.put(DELETED, user.isDeleted());
 //        log.info(claims.get("userId",Long.class).toString());
         // Check if the user has the TEST role
         if (user.getUserId() == 2L || user.getUserId() == 3L || (user.getUserId()> 14L && user.getUserId() < 29L)) {
