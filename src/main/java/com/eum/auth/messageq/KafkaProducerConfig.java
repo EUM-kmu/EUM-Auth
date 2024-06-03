1edc2d789e8696354009c9614e6e6709c8557577
solhee-hwang <solhee0912@kookmin.ac.kr>
2024-06-03 09:24:57 +0900

    chore : 리소스 변경

--- src/main/java/com/eum/auth/messageq/KafkaProducerConfig.java
+++ src/main/java/com/eum/auth/messageq/KafkaProducerConfig.java
@@ -21,7 +21,7 @@ public class KafkaProducerConfig {
     @Bean
     public ProducerFactory<String, String> producerFactory() {
         Map<String, Object> properties = new HashMap<>();
-        properties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "kafka:29092");
+        properties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "kafka:9092");
         properties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         properties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
 
